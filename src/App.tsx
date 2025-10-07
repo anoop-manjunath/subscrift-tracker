563b216a70e98ac9c3f6de7802d000d0b75977a5
Anoop DM <31535742+anoop-manjunath@users.noreply.github.com>
2025-10-07 10:42:15 +0530

    redirection changes

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const App = () => (
                 path="/dashboard"
                 element={
                   <ProtectedRoute>
-                    <Dashboard />
+                    <Index />
                   </ProtectedRoute>
                 }
               />
